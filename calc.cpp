ec39b1ccff5e686e760308c43920873162fe4f11
Asad Khan <asad.khan@matrixcomsec.com>
2025-02-04 06:42:10 +0000

    add basic calculation support

---
+++ calc.cpp
@@ -0,0 +1,21 @@
+// todo: calculator functionalities
+
+template <typename _Ty>
+_Ty add(_Ty a, _Ty b) {
+    return a + b;
+}
+
+template <typename _Ty>
+_Ty sub(_Ty a, _Ty b) {
+    return a - b;
+}
+
+template <typename _Ty>
+_Ty mul(_Ty a, _Ty b) {
+    return a * b;
+}
+
+template <typename _Ty>
+_Ty div(_Ty a, _Ty b) {
+    return a / b;
+}
